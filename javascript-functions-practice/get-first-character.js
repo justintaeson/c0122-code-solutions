37b08a830e1d9cb1f8ef008bc6fd17655c5e6b7b
Justin Son <justintaeson@gmail.com>
2022-02-01 20:27:43 -0800

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-first-character.js
@@ -0,0 +1,5 @@
+/* exported getFirstCharacter */
+function getFirstCharacter(string) {
+  var firstCharacter = string.substring(0, 1);
+  return firstCharacter;
+}
---
+++ javascript-functions-practice/get-square-of-number.js
@@ -0,0 +1,5 @@
+/* exported getSquareOfNumber */
+function getSquareOfNumber(number) {
+  var squareOfNumber = number ** 2;
+  return squareOfNumber;
+}
