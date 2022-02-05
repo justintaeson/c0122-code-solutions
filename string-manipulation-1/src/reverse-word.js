74ed22727b5deb7e67b5d430ca45de6516bc725b
Justin Son <justintaeson@gmail.com>
2022-02-04 16:31:30 -0800

    Updated answer to question: reverse-word.

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,17 +1,15 @@
 /* exported reverseWord */
 /*
 - defined a function to reverse any string that's inputted as a parameter
-- assigned a blank array to the reverseWordArray variable for to put characters in later
-- create a for loop to go through eacg letter of string and put it in our empty array called reverseWordArray
-- after the forloop has put all the characters in our array, the reverse method will reverse/flip all the letters in the array
-- last but not least, the function will return the reverseWordArray array with the all characters in the array joined together giving us a reversed version of the string that was inputted.
+- assigned a blank string to the emptyString variable to add character to it in our for loop
+- create a for loop to go through each letter of string and add it to our emptyString variable
+- last but not least, the function will return the emptyString variable with the all characters in the array joined together giving us a reversed version of the string that was inputted.
 */
 
 function reverseWord(word) {
-  var reverseWordArray = [];
-  for (let i = 0; i < word.length; i++) {
-    reverseWordArray.push(word.charAt(i));
+  var emptyString = '';
+  for (let i = word.length; i >= 0; i--) {
+    emptyString = emptyString + word.charAt(i);
   }
-  reverseWordArray.reverse();
-  return reverseWordArray.join('');
+  return emptyString;
 }
