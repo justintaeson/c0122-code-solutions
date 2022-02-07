be4676b18ed73a41f332fb2988991aee5179b6fa
Justin Son <justintaeson@gmail.com>
2022-02-07 10:41:53 -0800

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+var h1Element = document.querySelector('h1');
+console.log(h1Element);
+console.dir(h1Element);
+
+var explanationID = document.querySelector('#explanation');
+console.log(explanationID);
+console.dir(explanationID);
+
+var hintClass = document.querySelector('.hint');
+console.log(hintClass);
+console.dir(hintClass);
+
+var allPElements = document.querySelectorAll('p');
+console.log(allPElements);
+
+var exampleLinkClass = document.querySelectorAll('.example-link');
+console.log(exampleLinkClass);
