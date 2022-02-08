be9d786f486c582a93789de0282a86295cbbfff7
Justin Son <justintaeson@gmail.com>
2022-02-08 12:47:01 -0800

    Simplified conditional using else if statements.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,21 +6,17 @@ var $clickCount = document.querySelector('.click-count');
 function clickIncrement(event) {
   numberOfClicks++;
   $clickCount.textContent = 'Clicks: ' + numberOfClicks;
-  if (numberOfClicks >= 4 && numberOfClicks < 7) {
+  if (numberOfClicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (numberOfClicks < 7) {
     $hotButton.className = 'hot-button cool';
-  }
-  if (numberOfClicks >= 7 && numberOfClicks < 10) {
+  } else if (numberOfClicks < 10) {
     $hotButton.className = 'hot-button tepid';
-  }
-  if (numberOfClicks >= 10 && numberOfClicks < 13) {
+  } else if (numberOfClicks < 13) {
     $hotButton.className = 'hot-button warm';
-  }
-  if (numberOfClicks >= 13 && numberOfClicks < 16) {
+  } else if (numberOfClicks < 16) {
     $hotButton.className = 'hot-button hot';
-  }
-  if (numberOfClicks >= 16) {
-    $hotButton.className = 'hot-button nuclear';
-  }
+  } else { $hotButton.className = 'hot-button nuclear'; }
 }
 
 $hotButton.addEventListener('click', clickIncrement);
