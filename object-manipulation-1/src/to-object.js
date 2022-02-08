4bd8cd6aa2d251489982047fafc8e97ce9d47038
Justin Son <justintaeson@gmail.com>
2022-02-07 17:33:01 -0800

    Removed for loop in to object.js.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -2,19 +2,14 @@
 /*
 - defined a function to return a key value pair in an object from an inputted array with 2 elements
 - assigned an empty object called toObjectArray
-- created a for loop to iterate through the array
-- if the index of the element is either 0 or even
-- assign the current index of keyValuePair to a new variable called newKey
-- assign the value of newKey as the key key within the toObjectArray and the next index over as the value of the object
+- assigned index[0] to a new variable called newKey
+- assign the value of newKey as the key within the toObjectArray and the next index over as the value of the object
+- return the toObjectObject object ;)
 */
 
 function toObject(keyValuePair) {
-  var toObjectArray = {};
-  for (let i = 0; i < keyValuePair.length; i++) {
-    if (i === 0 || i % 2 === 0) {
-      var newKey = keyValuePair[i];
-      toObjectArray[newKey] = keyValuePair[i + 1];
-    }
-  }
-  return toObjectArray;
+  var toObjectObject = {};
+  var newKey = keyValuePair[0];
+  toObjectObject[newKey] = keyValuePair[1];
+  return toObjectObject;
 }
