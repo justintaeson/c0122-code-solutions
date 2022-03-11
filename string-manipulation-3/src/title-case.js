049a81444d1fe5296ba63f62ae4eed4a61e52be3
justintaeson <justintaeson@gmail.com>
2022-03-11 21:28:52 +0000

    Progress on problem #4: titleCase.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,28 @@
 /* exported titleCase */
+
+function titleCase(title) {
+  var array = [];
+  var subTitle = '';
+  var titleWithSpace = title + ' ';
+  for (let i = 0; i < titleWithSpace.length; i++) {
+    if (titleWithSpace[i] !== ' ' || titleWithSpace[i] !== ':' || titleWithSpace[i] !== '-') {
+      subTitle += titleWithSpace[i];
+    }
+    if (titleWithSpace[i] === ' ' || titleWithSpace[i] === ':' || titleWithSpace[i] === '-') {
+      array.push(subTitle);
+      subTitle = '';
+    }
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    array[i].slice(0, array[i].length);
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].toLowerCase() === 'javascript') {
+      array[i] = 'JavaScript';
+    } else if (array[i].toLowerCase() === 'api') {
+      array[i] = 'API';
+    }
+  }
+}
