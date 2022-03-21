8e5cf39efe99b8e578dd216ab82581a597280d54
justintaeson <justintaeson@gmail.com>
2022-03-21 05:54:44 +0000

    Completed express-delete exercise.

---
+++ express-delete/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+var grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const object in grades) {
+    gradesArray.push(grades[object]);
+  }
+  res.json(gradesArray);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[req.params.id];
+  res.send('Deleted id: ' + req.params.id);
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000!');
+});
