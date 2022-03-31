b7f5318f9e2b9d38c38ed8549d3d5318138a4011
justintaeson <justintaeson@gmail.com>
2022-03-31 22:35:57 +0000

    Completed react-toggle-switch-component challenge.

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -7,19 +7,30 @@ class ToggleSwitch extends React.Component {
   }
 
   handleClick() {
+
     this.setState(state => {
-      return { clicks: (this.state.clicked + 1) };
+      var clickCounter = { clicked: (state.clicked + 1) };
+      return clickCounter;
     });
   }
 
   render() {
-    if (this.state.clicked % 2 !== 0) {
+    if (this.state.clicked === 0 || this.state.clicked % 2 === 0) {
       return (
-        <div>
-        <div id="toggle-container" onClick={this.handleClick.bind(this)}>
-          <div className="toggle-slider"></div>
+        <div className='display-flex'>
+          <div id="toggle-container" onClick={this.handleClick.bind(this)}>
+            <div className="toggle-slider"></div>
+          </div>
+          <div id="label" onClick={this.handleClick.bind(this)}>OFF</div>
         </div>
-        <div id="label">OFF</div>
+      );
+    } else {
+      return (
+        <div className='display-flex'>
+          <div id="toggle-container" onClick={this.handleClick.bind(this)}>
+            <div className="toggle-slider position-right"></div>
+          </div>
+          <div id="label" onClick={this.handleClick.bind(this)}>ON</div>
         </div>
       );
     }
