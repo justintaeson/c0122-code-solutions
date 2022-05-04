68ef4c3d3caa1d33410d171f07a5cfd19a674812
justintaeson <justintaeson@gmail.com>
2022-05-04 19:04:22 +0000

    Completed react-carousel-component challenge.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -10,19 +10,63 @@ export default class Carousel extends React.Component {
   }
 
   handleClick(event) {
-    var arrowId = event.target.id;
-    if (arrowId === 'left-arrow') {
-      this.setState({ currentIndex: this.currentIndex - 1 });
+    if (event.target.id === 'left-arrow') {
+      if (this.state.currentIndex === 0) {
+        this.setState({ currentIndex: 4 });
+      } else {
+        this.setState({ currentIndex: this.state.currentIndex - 1 });
+      }
     }
-    if (arrowId.Id === 'right-arrow') {
-      this.setState({ currentIndex: this.currentIndex + 1 });
+    if (event.target.id === 'right-arrow') {
+      if (this.state.currentIndex === 4) {
+        this.setState({ currentIndex: 0 });
+      } else {
+        this.setState({ currentIndex: this.state.currentIndex + 1 });
+      }
     }
+    if (event.target.className === 'fa-regular fa-circle circle') {
+      this.setState({ currentIndex: event.target.id });
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.currentIndex === 4) {
+        this.setState({ currentIndex: 0 });
+      } else {
+        this.setState({ currentIndex: this.state.currentIndex + 1 });
+      }
+    }, 3000);
   }
 
   render() {
+    let className = '';
+    const circles = this.props.data.map(pokemon => {
+      if (pokemon.id === parseInt(this.state.currentIndex) || pokemon.id === this.state.currentIndex) {
+        className = 'fa-solid fa-circle circle';
+      } else {
+        className = 'fa-regular fa-circle circle';
+      }
+      return <i onClick={this.handleClick} className={className} id={pokemon.id} key={pokemon.id}></i>;
+    });
+
     return (
-      <div></div>
+    <>
+        <div id="pokemon" className='row'>
+        <div className='column-one-tenth'>
+            <i id="left-arrow" className="fa-solid fa-angle-left" onClick={this.handleClick}></i>
+        </div>
+        <div className='column-four-fifths'>
+          <img src={this.props.data[this.state.currentIndex].filepath} alt="Pokemon"/>
+        </div>
+        <div className='column-one-tenth'>
+            <i id="right-arrow" className="fa-solid fa-angle-right" onClick={this.handleClick}></i>
+        </div>
+      </div>
+      <div id="page-circles" className="row">
+        {circles}
+      </div>
+    </>
     );
   }
-
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import Carousel from './carousel';
 
 const images = [
-  '../images/coronavirus.jpeg',
-  '../images/lakers.jpeg',
-  '../images/oscars.jpeg',
-  '../images/russia-ukraine.png',
-  '../images/superbowl.webp',
-  '../winter-olympics.jpeg'
+  {
+    filepath: '../images/001.png',
+    id: 0
+  },
+  {
+    filepath: '../images/004.png',
+    id: 1
+  },
+  {
+    filepath: '../images/007.png',
+    id: 2
+  },
+  {
+
+    filepath: '../images/025.png',
+    id: 3
+  },
+  {
+
+    filepath: '../images/039.png',
+    id: 4
+  }
 ];
 
-ReactDOM.render(<Carousel images = {images} />, document.querySelector('#root'));
+ReactDOM.render(<Carousel data={images}/>, document.querySelector('#root'));
